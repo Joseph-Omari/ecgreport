5ac20cf994eb0c1c4215fa5b822cddb132e2b925
joseph-omari <josephomari80@gmail.com>
2023-07-14 18:57:13 +0000

    y

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="text-2xl mx-auto font-bold text-center bg-yellow-500 max-w-xl p-2">
+        <h1>ECG - KASOA SOUTH REPORT SYSTEM</h1>
+        <h1>User Login</h1>
+      </div>
+      <div className="bg-red-600 p-1 max-w-xl mx-auto"></div>
+
+      <form
+        action=""
+        className="bg-blue-700 text-white text-2xl mx-auto max-w-xl p-4 flex items-center justify-center flex-col space-y-8"
+      >
+        <div className="space-x-6">
+          <label htmlFor="" className=" text-2xl font-semibold ">
+            Username
+          </label>
+          <input type="text" className="rounded-md p-2  " />
+        </div>
+
+        <div className="space-x-6">
+          <label htmlFor="" className=" text-2xl font-semibold ">
+            Password
+          </label>
+          <input type="text" className="rounded-md p-2  " />
+        </div>
+
+        <div>
+            <button className="bg-yellow-600 rounded-md p-1 px-6 font-semibold">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
