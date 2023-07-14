140797091afa2a922657950d1c0b4ea5118948af
joseph-omari <josephomari80@gmail.com>
2023-07-14 16:24:52 +0000

    VERSION 1.24

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -13,11 +13,16 @@ const getData = async () => {
     }
     return res.json();
   } catch (error) {
-    console.log("Error loading meters: ", error);
+    console.log("Error loading report: ", error);
   }
 };
 
-export default function report() {
+
+
+export default async function report() {
+  const {reportdata} =  getData();
+  // console.log(reportdata);
+
   return (
     <div>
       <div className="md:hidden h-[70vh] flex justify-center items-center text-red-800 text-3xl font-bold text-center">
@@ -28,11 +33,53 @@ export default function report() {
         <div className="p-4 mx-auto  justify-center text-center font-bold text-red-700 text-2xl">
           <h1>Report Recieved from Field</h1>
         </div>
-
         <div className="bg-slate-500 p-4 space-y-2 gap-4 text-left">
-          
+          <div className=" ">
+            <div className="max-w-sm bg-white text-black p-4 mx-auto rounded-xl">
+              <div>
+                <div>
+                  <h1>Group Number</h1>
+                </div>
+                <div>
+                  <h1>Customer Name</h1>
+                </div>
+                <div>
+                  <h1>Customer Contact</h1>
+                </div>
+                <div>
+                  <h1>Tarrif Class</h1>
+                </div>
+                <div>
+                  <h1>Activity</h1>
+                </div>
+                <div>
+                  <h1>Meter Number</h1>
+                </div>
+                <div>
+                  <h1>Readings</h1>
+                </div>
+                <div>
+                  <h1>Anomalys</h1>
+                </div>
+                <div>
+                  <h1>Flat Rate</h1>
+                </div>
+                <div>
+                  <h1>Digital Address</h1>
+                </div>
+                <div>
+                  <h1>Id Type</h1>
+                </div>
+                <div>
+                  <h1>Id Number</h1>
+                </div>
+                <div>
+                  <h1>Remarks</h1>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   );
