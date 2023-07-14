7a13e2187eee7a43e0f45fb063285db44b99af9a
joseph-omari <josephomari80@gmail.com>
2023-07-14 18:38:37 +0000

    1

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,20 +1,5 @@
-"use client";
 import React from "react";
-import Logo from "public/logo.PNG";
 
-function page() {
-  return (
-    <div>
-      <div className="flex flex-col items-center justify-center mx-auto mt-10">
-        <Image
-          src={Logo}
-          alt="ecgLogo"
-          className="flex justify-center  w-52 drop-shadow-lg animate-bounce"
-        />
-        <h1 className="font-bold text-2xl">KASOA SOUTH DISTRICT</h1>
-      </div>
-    </div>
-  );
+export default function page() {
+  return <div>page</div>;
 }
-
-export default page;
