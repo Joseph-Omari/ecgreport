c60105f20ed35b7bb0e431736808176b2f989382
joseph-omari <josephomari80@gmail.com>
2023-07-14 12:54:06 +0000

    version 1.14

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
+const getData = async () => {
+  try {
+    const res = await fetch(
+      "https://ecgreport-joseph-omari.vercel.app/api/Field",
+      {
+        cache: "no-store",
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch meters");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Error loading meters: ", error);
+  }
+};
+
 export default function report() {
-  return <div>report</div>;
+  return (
+    <div>
+      <div className="md:hidden h-[70vh] flex justify-center items-center text-red-800 text-3xl font-bold text-center">
+        <h1>Viewed by Management Team Only</h1>
+      </div>
+      
+    </div>
+  );
 }
