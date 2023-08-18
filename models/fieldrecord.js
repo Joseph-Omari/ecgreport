3a2234e8694a00e50bb0d7dc88f4bf5909884185
joseph-omari <josephomari80@gmail.com>
2023-08-18 09:03:01 +0000

    W6D5

--- models/fieldrecord.js
+++ models/fieldrecord.js
@@ -22,6 +22,6 @@ const fieldSchema = new Schema(
 );
 
 const Field =
-  mongoose.models.Field || mongoose.model("FieldReportW6D4", fieldSchema);
+  mongoose.models.Field || mongoose.model("FieldReportW6D5", fieldSchema);
 
 export default Field;
